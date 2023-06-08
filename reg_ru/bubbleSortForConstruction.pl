907cef48d563f2b6339a5c9214bd58b4af1743dd
Dmitriy Skvortsov <skvordmitriy@yandex.ru>
2023-06-08 14:53:39 +0300

    tst tsk

---
+++ reg_ru/bubbleSortForConstruction.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+my @ent_numb =( 1 , 5 , 7 , 9 , 3 , 4 , 2 ,44 , 32 , -1 , -121 , -12 );
+my @out;
+my $enter_cnt = @ent_numb;
+
+print "enter some numbers \n";
+print "@ent_numb\n";
+
+
+for ( my $j = 0 ; $j <= $enter_cnt -2 ; $j++ )
+{
+    for ( my $i = 0 ; $i <= $enter_cnt -2; $i++ )
+    {
+        if ( $ent_numb[ $i ] > $ent_numb[ $i+1 ] )
+        {
+            ( $ent_numb[ $i ], $ent_numb[ $i+1 ] ) = ( $ent_numb[ $i+1 ], $ent_numb[ $i ] ); 
+        }    
+    } 
+    @out = @ent_numb;
+}
+
+print "After bubble sort\n";
+print "@out\n";
